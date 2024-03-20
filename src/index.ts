61a967f451a5561da0d79e0ffe42118d06e382e1
Raphaël Duhen <raphael.duhen@ign.fr>
2024-03-20 17:43:03 +0100

    feat: basic entwine interface

--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,196 @@
 import * as itowns from "itowns";
 import * as three from "three";
+import proj4 from "proj4";
 
-var viewerDiv = document.getElementById("viewerDiv") as HTMLDivElement;
-var placement = {
-    coord: new itowns.Coordinates("EPSG:4326", 3.3792, 44.3335, 844),
-    tilt: 22,
-    heading: -180,
-    range: 2840,
-};
+const viewerDiv = document.getElementById("viewerDiv") as HTMLDivElement;
+// const renderer = new three.WebGL1Renderer({
+//     canvas: document.createElement("canvas"),
+// });
+//
+// viewerDiv.appendChild(renderer.domElement);
 
-var view = new itowns.GlobeView(viewerDiv, placement);
+proj4.defs(
+    "EPSG:3946",
+    "+proj=lcc +lat_0=46 +lon_0=3 +lat_1=45.25 +lat_2=46.75 +x_0=1700000 +y_0=5200000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
 
-var orthoSource = new itowns.TMSSource({
-    crs: "EPSG:3857",
-    isInverted: true,
-    format: "image/png",
-    url: "http://osm.oslandia.io/styles/klokantech-basic/${z}/${x}/${y}.png",
-    attribution: {
-        name: "OpenStreetMap",
-        url: "http://www.openstreetmap.org/",
-    },
-    // @ts-ignore
-    tileMatrixSet: "PM",
-});
+// var debugDiv = document.getElementById("debugDiv") as HTMLDivElement;
 
-var orthoLayer = new itowns.ColorLayer("Ortho", {
-    source: orthoSource,
-});
+var view = new itowns.View("EPSG:3946", viewerDiv);
+view.mainLoop.gfxEngine.renderer.setClearColor(0x000000, 1.0);
 
-view.addLayer(orthoLayer);
+const controls = new itowns.PlanarControls(view as itowns.PlanarView);
 
-var elevationSource = new itowns.WMTSSource({
-    url: "http://wxs.ign.fr/altimetrie/geoportail/wmts",
-    crs: "EPSG:4326",
-    // @ts-ignore
-    name: "ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES",
-    tileMatrixSet: "WGS84G",
-    format: "image/x-bil;bits=32",
-    tileMatrixSetLimits: {
-        11: {
-            minTileRow: 442,
-            maxTileRow: 1267,
-            minTileCol: 1344,
-            maxTileCol: 2683,
-        },
-        12: {
-            minTileRow: 885,
-            maxTileRow: 2343,
-            minTileCol: 3978,
-            maxTileCol: 5126,
-        },
-        13: {
-            minTileRow: 1770,
-            maxTileRow: 4687,
-            minTileCol: 7957,
-            maxTileCol: 10253,
-        },
-        14: {
-            minTileRow: 3540,
-            maxTileRow: 9375,
-            minTileCol: 15914,
-            maxTileCol: 20507,
-        },
-    },
-});
+var eptUrl = document.getElementById("ept_url") as HTMLInputElement;
+var eptSource: itowns.EntwinePointTileSource;
+var eptLayer: itowns.EntwinePointTileLayer;
 
-var elevationLayer = new itowns.ElevationLayer("MNT_WORLD", {
-    source: elevationSource,
-});
+function onLayerReady() {
+    var lookAt = new three.Vector3();
+    var size = new three.Vector3();
+    eptLayer.root.bbox.getSize(size);
+    eptLayer.root.bbox.getCenter(lookAt);
 
-view.addLayer(elevationLayer);
+    controls.groundLevel = eptLayer.root.bbox.min.z;
+    var position = eptLayer.root.bbox.min
+        .clone()
+        .add(size.multiply({ x: 0, y: 0, z: size.x / size.z }));
 
-const pointCloudSource = new itowns.C3DTilesSource({
-    url:
-        "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/" +
-        "master/3DTiles/lidar-hd-gorges-saint-chely-tarn/tileset.json",
-});
+    view.camera3D.position.copy(position);
+    view.camera3D.lookAt(lookAt);
+    (view.camera3D as itowns.OrientedImageCamera).updateProjectionMatrix();
 
-function isPoints(obj: three.Object3D): obj is three.Points {
-    return (obj as three.Points).isPoints !== undefined;
+    view.notifyChange(view.camera3D);
 }
 
-function updatePointCloudSize(
-    event: three.Event<string, itowns.C3DTilesLayer>,
-) {
-    const tileContent = event.target.object3d;
-    tileContent.traverse(function(obj: three.Object3D) {
-        if (isPoints(obj)) {
-            (obj.material as three.PointsMaterial).size = 3.0;
+function loadEPT(url: string, options: any) {
+    eptSource = new itowns.EntwinePointTileSource({ url });
+
+    if (eptLayer) {
+        view.removeLayer("Entwine Point Tile");
+        view.notifyChange();
+        eptLayer.delete();
+    }
+
+    const config = {
+        source: eptSource,
+        crs: view.referenceCrs,
+        ...options,
+    };
+    eptLayer = new itowns.EntwinePointTileLayer("Entwine Point Tile", config);
+
+    view.addLayer(eptLayer).then(onLayerReady);
+
+    function dblClickHandler(event: three.Vector2 | MouseEvent | TouchEvent) {
+        var pick = view.pickObjectsAt(event, 5, eptLayer);
+
+        for (const p of pick) {
+            console.info(
+                "Selected point #" +
+                p.index +
+                " in position (" +
+                p.object.position.x +
+                ", " +
+                p.object.position.y +
+                ", " +
+                p.object.position.z +
+                ") - node " +
+                p.object.userData.node.id,
+            );
         }
-    });
+    }
+
+    view.domElement.addEventListener("dblclick", dblClickHandler);
 }
 
-const pointCloudLayer = new itowns.C3DTilesLayer(
-    "gorges",
-    {
-        source: pointCloudSource,
-    },
-    view,
-);
+function readEPTURL() {
+    const urlParams = new URL(location.href).searchParams;
+    let url = eptUrl.value || urlParams.get("ept");
 
-pointCloudLayer.addEventListener(
-    itowns.C3DTILES_LAYER_EVENTS.ON_TILE_CONTENT_LOADED,
-    updatePointCloudSize,
-);
+    if (url) {
+        const options: any = {};
+        for (const key of urlParams.keys()) {
+            if (key !== "ept") {
+                options[key] = parseInt(urlParams.get(key), 10);
+            }
+        }
+        loadEPT(url, options);
 
-itowns.View.prototype.addLayer.call(view, pointCloudLayer);
+        const entwineShare = document.getElementById("entwineShare");
+        if (entwineShare.hasAttribute("disabled")) {
+            entwineShare.removeAttribute("disabled");
+        }
+        document
+            .getElementById("entwineShareButton")
+            .addEventListener("click", function() {
+                const url =
+                    location.href.replace(location.search, "") +
+                    "?ept=" +
+                    eptUrl.value;
+                (
+                    document.getElementById(
+                        "entwineShareOutput",
+                    ) as HTMLInputElement
+                ).value = url;
+            });
+
+        eptUrl.value = url;
+    }
+}
+
+document
+    .getElementById("entwineLoadButton")
+    .addEventListener("click", readEPTURL);
+
+export function loadGrandLyon() {
+    eptUrl.value =
+        "https://download.data.grandlyon.com/files/grandlyon/imagerie/mnt2018/lidar/ept/";
+    readEPTURL();
+}
+
+document
+    .getElementById("entwineGrandLyonButton")
+    .addEventListener("click", loadGrandLyon);
+
+readEPTURL();
+
+// var placement = {
+//     coord: new itowns.Coordinates("EPSG:4326", 3.3792, 44.3335, 844),
+//     tilt: 22,
+//     heading: -180,
+//     range: 2840,
+// };
+//
+// var view = new itowns.GlobeView(viewerDiv, placement);
+//
+// const pointCloudSource = new itowns.C3DTilesSource({
+//     url:
+//         "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/" +
+//         "master/3DTiles/lidar-hd-gorges-saint-chely-tarn/tileset.json",
+// });
+//
+// function isPoints(obj: three.Object3D): obj is three.Points {
+//     return (obj as three.Points).isPoints !== undefined;
+// }
+//
+// function updatePointCloudSize(
+//     event: three.Event<string, itowns.C3DTilesLayer>,
+// ) {
+//     const tileContent = event.target.object3d;
+//     tileContent.traverse(function(obj: three.Object3D) {
+//         if (isPoints(obj)) {
+//             (obj.material as three.PointsMaterial).size = 3.0;
+//         }
+//     });
+// }
+//
+// const pointCloudLayer = new itowns.C3DTilesLayer(
+//     "gorges",
+//     {
+//         source: pointCloudSource,
+//     },
+//     view,
+// );
+//
+// pointCloudLayer.addEventListener(
+//     itowns.C3DTILES_LAYER_EVENTS.ON_TILE_CONTENT_LOADED,
+//     updatePointCloudSize,
+// );
+//
+// viewerDiv.addEventListener("click", function(event) {
+//     console.log(event);
+//     let picked = pointCloudLayer.pickObjectsAt(
+//         view,
+//         view.eventToViewCoords(event),
+//     );
+//     console.log(picked);
+//
+//     let now = new Date(Date.now());
+//     let hours = `${now.getHours()}`.padStart(2, "0");
+//     let minutes = `${now.getMinutes()}`.padStart(2, "0");
+//     let seconds = `${now.getSeconds()}`.padStart(2, "0");
+//     let tt = `${hours}:${minutes}:${seconds}`;
+//     debugDiv.innerHTML = `[${tt}] Picked`;
+// });
+//
+// itowns.View.prototype.addLayer.call(view, pointCloudLayer);
